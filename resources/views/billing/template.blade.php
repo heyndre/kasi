e81b73e40dfe3fd7f42376b6149d610e8cf44e77
pandegaf <pandegaf@gmail.com>
2023-12-31 04:36:37 +0700

    minor fix

--- resources/views/billing/template.blade.php
+++ resources/views/billing/template.blade.php
@@ -34,10 +34,13 @@
                 <img src="{{storage_path('app/public/logo-small.png')}}" alt="" srcset="" class="block h-16 w-auto">
             </a>
         </div>
-        <div class="text-2xl font-bold drop-shadow-md">
-            {{-- <span class="drop-shadow-md"> --}}
-                Tagihan Kelas KASI
-                {{-- </span> --}}
+        <div class="">
+            <div class="text-2xl font-bold drop-shadow-md">
+                {{-- <span class="drop-shadow-md"> --}}
+                    Tagihan Kelas KASI - {{str_pad($billing->invoice_id, 5, '0', STR_PAD_LEFT)}}
+                    {{-- </span> --}}
+            </div>
+            <div class="text-sm font-thin mt-2">Tagihan dibuat pada {{now()->format('d/m/Y H:i:s T')}}</div>
         </div>
     </div>
     <hr class="" style="border-color:#e36c2a">
@@ -47,6 +50,9 @@
             <div class="italic text-gray-800 text-sm">
                 Terima kasih telah belajar bersama KASI!
             </div>
+            @php
+                // setlocale(LC_ALL, 'id_ID');
+            @endphp
             <div class="text-gray-800 text-sm mt-6">
                 Berikut rincian tagihan murid a.n. :
                 <div class="px-4 py-2 font-semibold bg-amber-100 rounded-md my-1">
@@ -77,28 +83,28 @@
                     $totalPrice = 0;
                     $totalHours = 0;
                     $hours = 0;
-                    $i=0;
+                    $i=1;
                     @endphp
                     {{-- @for ($i = 0; $i < 15; $i++) @php $totalPrice +=$price; $totalHours +=$hours; @endphp --}}
-                        @foreach ($billing->theClass as $item) 
+                        @foreach ($billing->theClass as $item)
                         @php
-                            $totalPrice += $item->price;
-                            $totalHours += $item->length;
+                        $totalPrice += $item->price;
+                        $totalHours += $item->length;
                         @endphp
                         <tr class="border-b border-amber-800 dark:border-gray-700">
-                        <th scope="row" class="p-1 font-medium whitespace-nowrap bg-amber-600 text-white ">
-                            {{$i+1}}
-                        </th>
-                        <td class="p-2 bg-amber-100">
-                            {{$item->date_of_event->format('d/m/y')}} - {{$item->theTutor->userData->nickname}}
-                            {{-- Gerund {{Str::random(32)}} --}}
-                        </td>
-                        <td class="p-2 bg-amber-600 text-white">
-                            {{$item->length}} mnt
-                        </td>
-                        <td class="p-2 bg-amber-100">
-                            Rp.{{number_format($item->price, 0, ',', '.')}}
-                        </td>
+                            <th scope="row" class="p-1 font-medium whitespace-nowrap bg-amber-600 text-white ">
+                                {{$i++}}
+                            </th>
+                            <td class="p-2 bg-amber-100">
+                                {{$item->date_of_event->format('d/m/y')}} - {{$item->theTutor->userData->nickname}}
+                                {{-- Gerund {{Str::random(32)}} --}}
+                            </td>
+                            <td class="p-2 bg-amber-600 text-white">
+                                {{$item->length}} menit
+                            </td>
+                            <td class="p-2 bg-amber-100">
+                                Rp.{{number_format($item->price, 0, ',', '.')}}
+                            </td>
                         </tr>
                         @endforeach
                         {{-- @endfor --}}
