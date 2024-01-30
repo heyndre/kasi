b2670c9953cff95047e6835c809565d9225d02b9
pandegaf <pandegaf@gmail.com>
2024-01-30 10:24:41 +0700

    add favicon on welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,10 @@
 
   gtag('config', 'G-VRHMB694FN');
     </script>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('site.webmanifest')}}">
     <title>Portal KASI</title>
 
     <!-- Fonts -->
